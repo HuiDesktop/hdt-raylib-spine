d3005a561c1a1821da13578992e2832bd0c7bd68
Pure01Fx <pure01fx@outlook.com>
2022-06-11 09:02:00 +0800

    feat: successfully built lib

---
+++ hdtldecl.h
@@ -0,0 +1,5 @@
+#ifndef LUAFFIEXPORT
+#define HDTLAPI __declspec(dllexport)
+#else
+#define HDTLAPI
+#endif
---
+++ spine-raylib.h
@@ -0,0 +1,8 @@
+#include <spine/extension.h>
+#include <raylib.h>
+#include <raygui.h>
+#include <rlgl.h>
+#include <stdio.h>
+#include "hdtldecl.h"
+
+HDTLAPI void drawSkeleton(spSkeleton* skeleton, Vector3 position, bool PMA);
